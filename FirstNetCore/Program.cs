9c75cc1d896d07bf0abe20349ace339c297e6b6e
stepin105111 <paulshubham6@gmail.com>
2020-10-20 17:27:59 +0530

    firstcommit

---
+++ FirstNetCore/Program.cs
@@ -0,0 +1,23 @@
+﻿uusing System;
+
+namespace FirstNetCoreApp
+{
+    public delegate int MathOpDelegate(int M,int N);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(DateTime.Now.ToString());
+            MathOpDelegate delAdd = Add;
+            int result = delAdd(55,32);
+            Console.WriteLine("Result of Add:\t"+ result.ToString());
+        }
+    }
+
+    static int Add(int X, int Y){
+        return X+Y;
+    }
+
+}
+
